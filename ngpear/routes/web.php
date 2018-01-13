d0e6569c1a8ae1d7b370b1b959cb55e97e635557
ngpear <157199375@qq.com>
2018-01-13 19:08:47 +0800

    嵌套列表模板

--- ngpear/routes/web.php
+++ ngpear/routes/web.php
@@ -19,6 +19,21 @@ Route::Group(['prefix'=>'admin','namespace'=>'Admin'],function(){
     Route::get('/','IndexController@index');
     Route::get('welcome','IndexController@welcome');
     Route::resource('article','ArticleController');
+    Route::resource('picture','PictureController');
+    Route::resource('brand','BrandController');
+    Route::resource('category','CategoryController');
+    Route::resource('product','ProductController');
+    Route::resource('comment','CommentController');
+    Route::resource('member','MemberController');
+    Route::resource('level','LevelController');
+    Route::resource('score','ScoreController');
+    Route::resource('record','RecordController');
+    Route::resource('role','RoleController');
+    Route::resource('permission','PermissionController');
+    Route::resource('admin','AdminController');
+    Route::resource('system','SystemController');
+    Route::resource('system_category','SystemCategoryController');
+    Route::resource('system_log','SystemLogController');
 
 
 });
